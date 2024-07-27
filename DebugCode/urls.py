241348101f3c249cb35d98f4d478e3be9a0dbdbd
MariaVictoria <123131515+MariaVictoria@users.noreply.github.com>
2024-07-27 02:53:24 -0300

    SUBO ACTUALIZACIONES DE SEARCHING

--- DebugCode/urls.py
+++ DebugCode/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('home/', views.home, name='home'),
     path('searchingServices/', views.searchingServices, name='searchingServices'),
     path('search/', views.search, name='search'),
+    path('contact/', views.contact, name='contact'),
 ]
--- DebugCode/views.py
+++ DebugCode/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from gestionPedidos.models import Servicios
 
 def home(request):
     return render(request, 'home.html', {})
@@ -7,6 +8,22 @@ def home(request):
 def searchingServices(request):
     return render(request, 'searching.html', {})
 
+
 def search(request):
-    mensaje = f'Servicio buscado: {request.GET.get("ss", "")}'
-    return HttpResponse(mensaje)
+    serv = request.GET.get('ss', '')
+
+    if serv:
+        if len(serv) > 15:
+            mensaje = 'Búsqueda demasiado extensa. Por favor, ingrese una búsqueda más corta.'
+            return HttpResponse(mensaje)
+        else:
+            servicios = Servicios.objects.filter(seccion__icontains=serv)
+            return render(request, 'encontrados.html', {'servicios': servicios, 'query': serv})
+    else:
+        mensaje = 'No se encontró el Servicio'
+        return HttpResponse(mensaje)
+
+def contact(request):
+    if request.method == 'POST':
+        return render (request, 'gracias.html')
+    return render(request, 'contact.html', {})
